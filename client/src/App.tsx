ec8912fe305174d18ef77ca83499254da2de8944
Njenga Ian Josh <joshnjenga066@gmail.com>
2025-10-04 02:23:53 +0300

    Improved he UI, used the zustand

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Login, { Logout, Register } from "./pages/createAdmin";
 
 function App() {
 	return (
-		<main className=" box-border p-4 shadow-lg rounded-lg m-4 w-full min-h-screen ">
+		<main className=" box-border p-4 shadow-lg rounded-lg m-4 w-full min-h-screen  align-center">
 			<BrowserRouter>
 				<Navbar />
 
